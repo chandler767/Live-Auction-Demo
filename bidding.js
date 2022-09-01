db4ab1ebd03e44b887f2f26745b91101bff7f1bd
Chandler Mayo <chandler@chandlermayo.com>
2022-09-01 13:52:46 -0400

    Update bidding.js

--- bidding.js
+++ bidding.js
@@ -497,18 +497,4 @@ const updateUI = async () => {
     }
    
     
-   /* document.getElementById("btn-logout").disabled = !isAuthenticated
-    document.getElementById("btn-login").disabled = isAuthenticated
-    // NEW - add logic to show/hide gated content after authentication
-    if (isAuthenticated) {
-      document.getElementById("gated-content").classList.classList.remove("hidden")
-      document.getElementById(
-        "ipt-access-token"
-      ).innerHTML = await auth0.getTokenSilently()
-      document.getElementById("ipt-user-profile").innerHTML = JSON.stringify(
-        await auth0.getUser()
-      )
-    } else {
-      document.getElementById("gated-content").classList.add("hidden")
-    }*/
   }
